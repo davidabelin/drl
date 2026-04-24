85163958f130389317b4a8190a45400a2581862e
David Abelin <davidabelin@hotmail.com>
2026-04-23 23:03:07 -0600

    GAC for deeprl

--- scripts/drl_cloud_env.bat
+++ scripts/drl_cloud_env.bat
@@ -18,6 +18,7 @@ set "BUCKET_NAME=deeprl-031026-drl-data"
 set "SERVICE_NAME=drl-web"
 set "CANONICAL_DRL_URL=https://deeprl-031026.wm.r.appspot.com"
 set "CREDS_PATH=C:\Users\David\Documents\Local_Data\creds"
+set "GOOGLE_APPLICATION_CREDENTIALS=%CREDS_PATH%\deeprl-031026-dc9a9d98c6c6.json"
 set "RUN_MEMORY=1Gi"
 set "RUN_CPU=1"
 exit /b 0
